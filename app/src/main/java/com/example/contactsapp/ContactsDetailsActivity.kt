ec8271c326d5354c3b474ab5da4cc70d547d8b50
Yosef Haj <yosefterawi@hotmail.com>
2024-03-10 19:25:51 +0200

    UI for Contacts Details Activity only is build and looks good

--- app/src/main/java/com/example/contactsapp/ContactsDetailsActivity.kt
+++ app/src/main/java/com/example/contactsapp/ContactsDetailsActivity.kt
@@ -1,11 +1,14 @@
 package com.example.contactsapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.contactsapp.databinding.ActivityContactsDetailsBinding
 
 class ContactsDetailsActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityContactsDetailsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_contacts_details)
+        binding = ActivityContactsDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }

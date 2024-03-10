d11b0116c57c01eccae5c5a58aed109f3160dcfb
Yosef Haj <yosefterawi@hotmail.com>
2024-03-10 16:53:54 +0200

    first commit

---
+++ app/src/main/java/com/example/contactsapp/ContactsDetailsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.contactsapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ContactsDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contacts_details)
+    }
+}
